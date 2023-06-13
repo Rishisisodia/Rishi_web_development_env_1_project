865ce46af984374fc2520320c540227ee5ba5b92
Rishisisodia <rishisisodia9900@gmail.com>
2023-06-13 09:27:58 -0400

    Changes has been added

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+	<header>
+		<div class="main-header">
+		<div class="container">
+			<div class="header-flex">
+				<div class="logo">
+					<a href="index.php">Restaurant</a>
+				</div>
+				<nav>
+					<ul class="nav-list">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="destination.html">Destination</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		</div>
+	</header>
+	<main>
+		<section class="big-banner">
+		<div class="box">
+			<h2>wide option of choice</h2>
+			<h1>delicious recipes</h1>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Ex voluptatum, facere, nulla sit minima</p>
+			<a href="" class="btn1">check our menu</a>
+		</div>
+	</section>
+
+	<section class="product">
+		<div class="container">
+			<div class="product-flex">
+				<div class="left">
+					<h1>about our story</h1>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe nostrum minus doloremque perferendis officia soluta distinctio aliquam. Voluptatem, minima nisi, animi laboriosam eius id sint deleniti molestiae, dolores nam odio!</p>
+					<a href="" class="btn2">view full menu</a>
+				</div>
+				<div class="right">right</div>
+			</div>
+		</div>
+	</section>
+
+
+	</main>
+	<footer></footer>
+</body>
